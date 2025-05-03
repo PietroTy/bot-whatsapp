a349d77ef9a01102d33e720c3514ae2a9366f495
PietroTy <pietro.turcimm@gmail.com>
2025-05-03 18:35:23 -0300

    bot configurado para grupo de figurinhas

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//PietroTy//
+// PietroTy //
 require('dotenv').config();
 
 const { Client } = require('whatsapp-web.js');
@@ -27,13 +27,22 @@ client.on('ready', () => {
 
 client.on('message', async (message) => {
     const text = message.body.toLowerCase();
+    const groupName = "figurinhas#bot";
 
     if (text.length < 2) return;
 
-    if (message.hasQuotedMsg && text.startsWith("#bot")) {
-        try {
-            const quotedMessage = await message.getQuotedMessage();
+    const chat = await message.getChat();
+    const isStickerGroup = chat.isGroup && chat.name === groupName;
 
+    if (isStickerGroup && text.trim() === "#link") {
+        await message.reply("Aqui está o link do grupo:\nhttps://chat.whatsapp.com/KAg83JlOyWSGoHLBOLwrR8");
+        return;
+    }
+
+    if (message.hasQuotedMsg && text.startsWith("#bot")) {
+        const quotedMessage = await message.getQuotedMessage();
+
+        if (isStickerGroup) {
             if (quotedMessage.hasMedia && quotedMessage.type === 'image') {
                 const media = await quotedMessage.downloadMedia();
 
@@ -60,30 +69,42 @@ client.on('message', async (message) => {
                     stickerAuthor: ''
                 });
             } else {
+                await message.reply("Neste grupo, apenas comandos de criação de figurinhas com imagens são permitidos.");
+            }
+            return;
+        }
+
+        try {
+            const media = await quotedMessage.downloadMedia();
+
+            if (quotedMessage.hasMedia && quotedMessage.type === 'image') {
+                if (!media || !media.mimetype.startsWith("image")) {
+                    await message.reply("Isso não parece ser uma imagem compatível.");
+                    return;
+                }
+
+                const buffer = Buffer.from(media.data, 'base64');
+
+                const webpBuffer = await sharp(buffer)
+                    .resize(512, 512, {
+                        fit: 'contain',
+                        background: { r: 0, g: 0, b: 0, alpha: 0 }
+                    })
+                    .webp()
+                    .toBuffer();
+
+                const stickerMedia = new MessageMedia('image/webp', webpBuffer.toString('base64'));
+
+                await client.sendMessage(message.from, stickerMedia, {
+                    sendMediaAsSticker: true,
+                    stickerName: '',
+                    stickerAuthor: ''
+                });
+            } else {
                 const userId = message.from;
 
                 if (!chatHistory[userId]) {
-                    chatHistory[userId] = [{
-                        role: "system",
-                        content: `Você é um robô chamado "Bot". Responda de forma direta, objetiva e robótica, sem usar gírias, emojis ou linguagem informal. Sua função é auxiliar quando chamado com frases que iniciam com "#bot", realizando as seguintes tarefas:
-                        
-                        - Responder perguntas objetivas e gerais.
-                        - Gerar textos sob demanda (como resumos, descrições, frases e ideias).
-                        - Manter conversas contínuas com base no histórico.
-                        - Criar figurinhas: ao receber uma imagem com a legenda exatamente "#bot", você deve transformá-la em figurinha.
-                        - Trazer informações sobre o servidor de Minecraft, sobre o criador do bot e links relevantes.
-                        
-                        Adicionalmente, dependendo do contexto:
-                        
-                        - Informe suas funcionalidades de forma resumida quando questionado sobre elas.
-                        - Quando a conversa mencionar "Minecraft", "Pitcraft" ou similares, mencione o servidor de Minecraft de IP: pitcraft.duckdns.org:13377.
-                        - Quando perguntado, informe a seed do mapa: 7572138602753151096.
-                        - Instrua o usuário a digitar "/l opa" no chat ao entrar no servidor para realizar login.
-                        - Quando perguntado, informe que seu criador é Pietro Turci, estudante de Ciência da Computação no IFSP.
-                        - Quando questionado, forneça os seguintes links:
-                            - GitHub: https://github.com/PietroTy
-                            - Portfólio: https://pietroty.github.io/PietroTy/`
-                    }];
+                    chatHistory[userId] = [getSystemPrompt()];
                 }
 
                 const contextMessage = quotedMessage.body || "Mensagem sem texto.";
@@ -141,31 +162,16 @@ client.on('message', async (message) => {
     }
 
     else if (text.startsWith("#bot")) {
+        if (isStickerGroup) {
+            await message.reply("Neste grupo, apenas comandos de criação de figurinhas são permitidos.");
+            return;
+        }
+
         try {
             const userId = message.from;
 
             if (!chatHistory[userId]) {
-                chatHistory[userId] = [{
-                    role: "system",
-                    content: `Você é um robô chamado "Bot". Responda de forma direta, objetiva e robótica, sem usar gírias, emojis ou linguagem informal. Sua função é auxiliar quando chamado com frases que iniciam com "#bot", realizando as seguintes tarefas:
-                    
-                    - Responder perguntas objetivas e gerais.
-                    - Gerar textos sob demanda (como resumos, descrições, frases e ideias).
-                    - Manter conversas contínuas com base no histórico.
-                    - Criar figurinhas: ao receber uma imagem com a legenda exatamente "#bot", você deve transformá-la em figurinha.
-                    - Trazer informações sobre o servidor de Minecraft, sobre o criador do bot e links relevantes.
-                    
-                    Adicionalmente, dependendo do contexto:
-                    
-                    - Informe suas funcionalidades de forma resumida quando questionado sobre elas.
-                    - Quando a conversa mencionar "Minecraft", "Pitcraft" ou similares, mencione o servidor de Minecraft de IP: pitcraft.duckdns.org:13377.
-                    - Quando perguntado, informe a seed do mapa: 7572138602753151096.
-                    - Instrua o usuário a digitar "/l opa" no chat ao entrar no servidor para realizar login.
-                    - Quando perguntado, informe que seu criador é Pietro Turci, estudante de Ciência da Computação no IFSP.
-                    - Quando questionado, forneça os seguintes links:
-                        - GitHub: https://github.com/PietroTy
-                        - Portfólio: https://pietroty.github.io/PietroTy/`
-                }];
+                chatHistory[userId] = [getSystemPrompt()];
             }
 
             chatHistory[userId].push({ role: "user", content: text });
@@ -186,6 +192,34 @@ client.on('message', async (message) => {
     }
 });
 
+function getSystemPrompt() {
+    return {
+        role: "system",
+        content: `Você é um robô chamado "Bot". Responda de forma direta, objetiva e robótica, sem usar gírias, emojis ou linguagem informal, e de preferência em formato de tópicos.
+        Sua função é auxiliar quando chamado com frases que iniciam com "#bot", realizando as seguintes tarefas:
+        
+        - Responder perguntas objetivas e gerais.
+        - Gerar textos sob demanda (como resumos, descrições, frases e ideias).
+        - Manter conversas contínuas com base no histórico.
+        - Criar figurinhas: ao receber uma imagem com a legenda exatamente "#bot", você deve transformá-la em figurinha.
+        - Informar da existência e mandar o link do grupo chamado "figurinhas#bot" quando solicitado: https://chat.whatsapp.com/KAg83JlOyWSGoHLBOLwrR8.
+        - Responder perguntas sobre o servidor de Minecraft, como IP, seed e login.
+        - Responder perguntas sobre o criador do bot, como nome e links relevantes.
+        
+        Adicionalmente, dependendo do contexto:
+        
+        - Informe suas funcionalidades de forma resumida quando questionado sobre elas.
+        - Quando a conversa mencionar "Minecraft", "Pitcraft" ou similares, mencione o servidor de Minecraft de IP: pitcraft.duckdns.org:13377.
+        - Quando perguntado, informe a seed do mapa: 7572138602753151096.
+        - Instrua o usuário a digitar "/l opa" no chat ao entrar no servidor para realizar login.
+        - Quando perguntado, informe que seu criador é Pietro Turci, estudante de Ciência da Computação no IFSP.
+        - Quando questionado, forneça os seguintes links:
+            - GitHub: https://github.com/PietroTy
+            - Portfólio: https://pietroty.github.io/PietroTy/`
+    };
+}
+
+// Função de chamada à IA
 async function perguntarIA(messages) {
     const response = await axios.post(
         'https://openrouter.ai/api/v1/chat/completions',
@@ -205,3 +239,12 @@ async function perguntarIA(messages) {
 }
 
 client.initialize();
+
+client.on('disconnected', (reason) => {
+    console.log('Bot desconectado:', reason);
+    client.initialize();
+});
+
+client.on('authenticated', (session) => {
+    console.log('Bot autenticado!');
+});
