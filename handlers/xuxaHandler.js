bbffb4fc7a4804537aa0c2bdd16ee6bbcd9ca78d
PietroTy <pietro.turcimm@gmail.com>
2026-08-13 23:54:13 -0300

    style: altera tema livre para Tema Livre (Qualquer bosta)

--- handlers/xuxaHandler.js
+++ handlers/xuxaHandler.js
@@ -22,7 +22,7 @@ const THEMES = [
     "Esportes ou Atletas",
     "Vilões de Filmes ou Desenhos",
     "Frutas, Verduras ou Legumes",
-    "Tema Livre (Qualquer palavra ou conceito)"
+    "Tema Livre (Qualquer bosta)"
 ];
 
 const ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
